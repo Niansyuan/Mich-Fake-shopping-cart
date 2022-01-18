e31291a04c695ed1209b3afae4b4b976f281f912
LAPTOP-IUJTRKTD\Niansyuan <mich8588@gmail.com>
2022-01-18 18:52:47 +0800

    fetch Fake store API

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,26 @@ import { Drawer, LinearProgress, Grid, Badge } from "@mui/material";
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 //styles
 import { Wrapper } from "./App.styles";
+//types
+export type CartItemType = {
+  id: number,
+  category: string,
+  description: string,
+  image: string,
+  price: number,
+  title: string
+};
+
+const getProducts = async (): Promise<CartItemType[]> =>
+  await (await fetch('https://fakestoreapi.com/products')).json();
 
 const App = () => {
+  const { data, isLoading, error } = useQuery<CartItemType[]>(
+    'products',
+    getProducts
+  );
+
+  console.log(data);
   return (
     <div className="App">
       start
